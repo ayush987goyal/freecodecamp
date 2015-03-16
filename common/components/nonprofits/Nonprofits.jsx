a59baace79602198dcb2d4f1f54a96fb97116e12
Michael Q Larson <michaelqlarson@gmail.com>
2015-03-15 17:31:36 -0700

    got jsx sample working for future reference

--- common/components/nonprofits/Nonprofits.jsx
+++ common/components/nonprofits/Nonprofits.jsx
@@ -5,12 +5,36 @@ var React = require('react'),
       Col,
     } = require('react-bootstrap');
 
-var Nonprofits = React.createClass({
-
+var Welcome = React.createClass({
   render: function () {
     return (
-      <h1>Hello Non Profits!</h1>
+      <Grid>
+        <div className="jumbotron text-center">
+          <h1 className="hug-top"> We Help Nonprofits</h1>
+          <h2>Well code software solutions for your nonprofit, for free!</h2>
+          <p>Step: {this.props.step}</p>
+        </div>
+      </Grid>
     );
+  }
+});
+
+var Nonprofits = React.createClass({
+  getInitialState: function () {
+    return {
+      step: 0
+    }
+  },
+
+  render: function () {
+    switch (this.state.step) {
+      case 0:
+        return <Welcome step={this.state.step} />;
+        break;
+      case 1:
+        return <h1>Page 2</h1>
+        break;
+    }
   }
 });
 
