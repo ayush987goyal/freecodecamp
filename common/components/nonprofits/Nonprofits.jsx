99e934217568934a07ecf53dd91ae519c23ff893
Michael Q Larson <michaelqlarson@gmail.com>
2015-03-15 17:57:48 -0700

    finish pair programming session with Foxed2 on nonprofit react

--- common/components/nonprofits/Nonprofits.jsx
+++ common/components/nonprofits/Nonprofits.jsx
@@ -6,13 +6,18 @@ var React = require('react'),
     } = require('react-bootstrap');
 
 var Welcome = React.createClass({
+
+  handleClick: function () {
+    this.props.stepPlusOne();
+  },
+
   render: function () {
     return (
       <Grid>
         <div className="jumbotron text-center">
           <h1 className="hug-top"> We Help Nonprofits</h1>
           <h2>Well code software solutions for your nonprofit, for free!</h2>
-          <p>Step: {this.props.step}</p>
+          <button onClick={this.handleClick}>Next Question</button>
         </div>
       </Grid>
     );
@@ -26,11 +31,15 @@ var Nonprofits = React.createClass({
     }
   },
 
+  incrementStep: function() {
+    this.setState({step: this.state.step++})
+  },
+
   render: function () {
     switch (this.state.step) {
       case 0:
-        return <Welcome step={this.state.step} />;
-        break;
+        return <Welcome stepPlusOne={this.incrementStep}/>;
+        break
       case 1:
         return <h1>Page 2</h1>
         break;
